c4f16d39d906a1c918d4d52a22ac50bdf9f9468a
Rasel Meya <>
2021-04-12 10:44:32 +0000

    list.py

---
+++ list.py
@@ -0,0 +1,21 @@
+name=["Dhaka", "Mymensingh","Noakhali", "Chattogram", "Khulna"]
+''' All name print '''
+print(name)
+''' individual name print'''
+print("\n")
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+''' print through reverse side'''
+print("\n")
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+print(name[-5])
+''' All name item print through iterative for loop'''
+print("\n")
+for item in name:
+  print(item)
